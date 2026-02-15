cb5013a9db3714f4695382937397fbade47034d9
juancastro007 <juan.castro07@utrgv.edu>
2026-02-15 01:04:45 +0000

    functions implemented and variables mutable but multiple errors in the if-else loops and also tracking guesses

---
+++ assignment3/src/main.rs
@@ -0,0 +1,62 @@
+// Juan Castro csci-3334 assign#3
+// Guessing game
+
+// 1. Use a mutable variable to store a "secret_num" number (you can hard-code this).
+
+// 2.Implement a function check_guess(guess: i32, secret_num: i32) -> i32 that returns:
+// 0 if the guess is correct
+// 1 if the guess is too high
+// -1 if the guess is too low
+
+// 3.In the main function:
+// Use a loop to repeatedly:
+// Set a mutable guess variable to a number of your choice (simulating user input)
+// Call the check_guess function
+// Use an if-else expression to print whether the guess was correct, too high, or too low
+// If the guess was correct, break the loop
+
+// After the loop ends, print how many guesses it took 
+// (you'll need to track this in a variable)
+
+fn check_guess(guess: i32, secret_num: i32) -> i32 {
+    if guess == secret_num {
+        0
+    }
+    else if guess > secret_num {
+        1
+    }
+    else {
+        -1
+    }
+}
+
+fn main () {
+    let mut secret_num = 7;
+
+    let mut guess_count = 0;
+
+    let guesses: [i32; 10] = [1,2,3,4,5,6,7,8,9,15];
+
+    let mut index = 0;
+
+    loop {
+        let guess = guesses[index];
+        guess_count += 1;
+        println!("Guess #{:?}: {}", guesses, guess);
+
+        let result = check_guess(guess, secret_num);
+
+        if result == 0 {
+            println!("Correct guess")
+        }
+        else if result == 1 {
+            println!("Guess is too high")
+        }
+        else if result == -1 {
+            println!("Guess is too low")
+        }
+        index += 1;
+    }
+
+    println!("Total guesses: {}", guess_count);
+}
---
+++ assignment3/target/debug/deps/assignment3-4265638c5743a70a.d
@@ -0,0 +1,5 @@
+/workspaces/csci-3334-spring2026/assignment3/target/debug/deps/assignment3-4265638c5743a70a.d: src/main.rs
+
+/workspaces/csci-3334-spring2026/assignment3/target/debug/deps/assignment3-4265638c5743a70a: src/main.rs
+
+src/main.rs:
