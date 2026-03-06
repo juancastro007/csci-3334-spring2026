c7855e1eb3e296af39e891cffbdeb9eee6ed8625
juancastro007 <juan.castro07@utrgv.edu>
2026-03-06 20:48:39 +0000

    book catalog todo's complete

--- book_catalog/src/main.rs
+++ book_catalog/src/main.rs
@@ -27,16 +27,42 @@ struct Book {
 fn save_books(books: &Vec<Book>, filename: &str) {
     // TODO: Implement this function
     // Hint: Use File::create() and write!() macro
+    let mut file = File::create(filename).expect("Could not create file");
+
+    for book in books {
+        writeln!(file, "{}, {}, {}", book.title, book.author, book.year).expect("Could not write to file");
+    }
 }
 
 fn load_books(filename: &str) -> Vec<Book> {
     // TODO: Implement this function
     // Hint: Use File::open() and BufReader
+    let file = File::open(filename).expect("Could not open file");
+    let reader = BufReader::new(file);
+
+    let mut books: Vec<Book> = Vec::new();
+
+    for line in reader.lines() {
+        let line = line.expect("Could not read line");
+
+        let parts: Vec<&str> = line.split(',').collect();
+
+        if parts.len() == 3 {
+            let book = Book {
+                title: parts[0].to_string(),
+                author: parts[1].to_string(),
+                year: parts[2].parse().expect("Invalid year"),
+            };
+            books.push(book);
+        }
+    }
+    books
 }
 
 fn main() {
     let books = vec![
         Book { title: "1984".to_string(), author: "George Orwell".to_string(), year: 1949 },
+        //Book { title: "green eggs and ham".to_string(), author: "dr. suess".to_string(), year: 55555 },
         Book { title: "To Kill a Mockingbird".to_string(), author: "Harper Lee".to_string(), year: 1960 },
     ];
 
---
+++ book_catalog/target/debug/book_catalog.d
@@ -0,0 +1,1 @@
+/workspaces/csci-3334-spring2026/book_catalog/target/debug/book_catalog: /workspaces/csci-3334-spring2026/book_catalog/src/main.rs
