9ebc3d78cab451e41c40a0819b74c0d74ff3bfe8
juancastro007 <juan.castro07@utrgv.edu>
2026-03-11 17:42:10 +0000

    in progress main

--- bank_account/src/main.rs
+++ bank_account/src/main.rs
@@ -1,3 +1,25 @@
+// following the steps as in the rust struct testing lab
+mod bank_account;
+use bank_account::BankAccount;
+
+use std::io // use standard input/output
+
 fn main() {
-    println!("Hello, world!");
+    let mut account = BankAccount::new(100.0);
+    
+    println!("Initial balance: {}", account.balance());
+
+    loop {
+        println!("Bank of Banking");
+        println!("1. Deposit");
+        println!("1. Withdraw");
+        println!("1. Balance");
+        println!("1. Exit");
+
+        //
+        let mut choice = String::new();
+        io:stdin().read_line(&mut choice).expect("Failed to read input");
+
+        let choice: i32 = choice.trim().parse().unwrap_or
+    }
 }
