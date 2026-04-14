81d80240725407bf885ba4a2191f6053872d5780
juancastro007 <juan.castro07@utrgv.edu>
2026-04-14 00:33:23 +0000

    in class 2/2

--- in_class_4_13/src/main.rs
+++ in_class_4_13/src/main.rs
@@ -1,3 +1,5 @@
+/*
+// Assignment #1
 use std::thread;
 use std::time::Duration;
 
@@ -26,4 +28,43 @@ fn main() {
     }
     
     println!("All threads completed.");
+}*/
+/////////////////////////////////////////////////////
+
+// Assignment #2
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+fn main() {
+    // Shared counter starting at 0
+    let counter = Arc::new(Mutex::new(0));
+
+    // Vector to store thread handles
+    let mut handles = vec![];
+
+    // Spawn 5 threads
+    for i in 1..=5 {
+        // Clone the Arc so each thread gets shared ownership
+        let counter_clone = Arc::clone(&counter);
+
+        let handle = thread::spawn(move || {
+            // Increment the counter 10 times
+            for _ in 0..10 {
+                let mut num = counter_clone.lock().unwrap();
+                *num += 1;
+            }
+
+            println!("Thread {} done", i);
+        });
+
+        handles.push(handle);
+    }
+
+    // Wait for all threads to complete
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    // Print the final value of the counter
+    println!("Final counter value: {}", *counter.lock().unwrap());
 }
